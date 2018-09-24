7bdaf51302291ebfb941579f16417ea359ce77a0
Chicago <chicago01@qq.com>
2018-09-24 11:43:45 +0800

    text

--- text.cpp
+++ text.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int num[100],tot;
-    cout<<tot<<endl;
-    num[tot]=1;
-    cout<<num[tot]<<endl;
-    num[++tot]=233;
-    cout<<num[tot]<<endl;
+    int min,n=3,m=12;
+    cout<<(n+m)/2<<endl;
+    
+    cout<<n+(m-n)/2<<endl;
+    
+    //cout<<num[tot]<<endl;
     
     getchar();getchar();getchar();
     return 0;
