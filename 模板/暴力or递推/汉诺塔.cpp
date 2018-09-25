7abfc8fde8150282eb0ddbc3a5185cdaa49dc2f5
Chicago <chicago01@qq.com>
2018-09-25 22:46:27 +0800

    过ccf题库

--- 模板/暴力or递推/汉诺塔.cpp
+++ 模板/暴力or递推/汉诺塔.cpp
@@ -1,17 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+int ans=0;
 inline void tata(int n,string from,string to,string help){
-    if(n == 1) cout << "Move " << n << ends << from << " to " << to <<endl;
+    if(n == 1) cout << ++ans << ".Move " << n <<" from "<< from << " to " << to <<endl;
     else{
         tata(n-1,from,help,to);
-        cout << "Move " << n << ends << from << " to " << to <<endl;
+        cout << ++ans <<".Move " << n <<" from "<< from << " to " << to <<endl;
         tata(n-1,help,to,from);
 
     }
 }
 int main(int argc, char const *argv[])
 {
-    tata(3,"a","b","c");
+    int n;
+    scanf("%d",&n);
+    tata(n,"a","c","b");
     getchar();getchar();
     return 0;
 }
