07a378553512488405a710b260b345515ffccf6d
Chicago <chicago01@qq.com>
2018-09-25 22:29:06 +0800

    正解

--- 模板/暴力or递推/字符串的所有子序列.cpp
+++ 模板/暴力or递推/字符串的所有子序列.cpp
@@ -1,2 +1,17 @@
 #include<bits/stdc++.h>
-char chuan
+using namespace std;
+inline void chuan(string ch,int i,string res){
+    if(i == ch.length()) {
+        cout << res << endl;
+        return;
+    }
+    chuan(ch,i+1,res+ch[i]);
+    chuan(ch,i+1,res);
+}
+
+int main(int argc, char const *argv[])
+{
+    chuan("abc",0,"");
+    getchar();
+    return 0;
+}
