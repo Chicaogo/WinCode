da115e1b5ab062558cf1f8734fd00b0e9d414fea
Chicago <chicago01@qq.com>
2018-09-26 21:57:15 +0800

    皮一拨

---
+++ 刷题/洛谷刷题/模拟/皮一拨.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"                   皮"<<endl;
+    cout<<"                   皮"<<endl;
+    cout<<"       皮皮皮皮皮皮皮皮皮皮皮皮皮"<<endl;
+    cout<<"       皮          皮        皮"<<endl;
+    cout<<"       皮          皮      皮"<<endl;
+    cout<<"      皮           皮"<<endl;
+    cout<<"      皮  皮皮皮皮皮皮皮皮皮"<<endl;
+    cout<<"      皮     皮        皮"<<endl;
+    cout<<"     皮        皮   皮"<<endl;
+    cout<<"    皮           皮"<<endl;
+    cout<<"   皮         皮     皮"<<endl;
+    cout<<"  皮       皮          皮"<<endl;
+    cout<<"皮     皮                皮皮皮"<<endl;
+    getchar();
+    return 0;
+}
