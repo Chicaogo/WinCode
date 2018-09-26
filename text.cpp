30be54f1b9eda8c1bb1e2deaede386a83b35eb6e
Chicago <chicago01@qq.com>
2018-09-26 22:06:35 +0800

    0

--- text.cpp
+++ text.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int min,n=3,m=12;
-    cout<<(n+m)/2<<endl;
     
-    cout<<n+(m-n)/2<<endl;
-    
-    //cout<<num[tot]<<endl;
+    cout<<"我爱你中国！"<<endl;
     
     getchar();getchar();getchar();
     return 0;
