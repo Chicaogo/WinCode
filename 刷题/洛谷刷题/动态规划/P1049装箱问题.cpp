e6cb6856c8d6e3a87491ad007f2f304799f91096
Chicago <chicago01@qq.com>
2018-10-08 00:11:35 +0800

    简单的dp

---
+++ 刷题/洛谷刷题/动态规划/P1049装箱问题.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXV = 20001;
+int n,V,dp[MAXV],v[31];
+
+int main(int argc, char const *argv[])
+{
+    cin >> V >> n;
+    
+    for(int i = 0;i < n;++i) cin >> v[i];
+
+    for(int i = 0;i < n;++i)
+    {
+        for(int j = V;j >= v[i];j--)
+        {
+            if(dp[j] < dp[j-v[i]]+v[i])
+                dp[j] = dp[j-v[i]]+v[i];
+        }
+    }
+
+    cout << V-dp[V];
+
+    getchar();getchar();
+    return 0;
+}
