637d2438ebd1be5ed825de4c4c28fa935d697948
Chicago <chicago01@qq.com>
2018-10-08 00:17:55 +0800

    增加注释

--- 刷题/洛谷刷题/动态规划/P1049装箱问题.cpp
+++ 刷题/洛谷刷题/动态规划/P1049装箱问题.cpp
@@ -2,6 +2,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+    这道题实际上还是一道0-1背包问题，存在两种情况，要或不要
+*/
+
 const int MAXV = 20001;
 int n,V,dp[MAXV],v[31];
 
@@ -12,6 +16,7 @@ int main(int argc, char const *argv[])
     for(int i = 0;i < n;++i) cin >> v[i];
 
     for(int i = 0;i < n;++i)
+    //从第一个背包开始枚举，要还是不要
     {
         for(int j = V;j >= v[i];j--)
         {
