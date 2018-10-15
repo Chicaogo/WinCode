74bcca03c19130d1866bc0f948e7f6e69797bb31
Chicago <chicago01@qq.com>
2018-10-15 12:10:56 +0800

    有bug

--- 模板/造数据/模板.cpp
+++ 模板/造数据/模板.cpp
@@ -1,8 +1,7 @@
-#include<cstdlib>
-#include<cstdio>
-#include<ctime>
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+const int inf = 1e5;
 
 int random(int n)
 {
@@ -36,6 +35,41 @@ inline interval()  //随机生成m <= 100 个[1,n]的子区间
     }
 }
 
+inline tree()  //随机生成n个点的树
+{
+    int n = random(100000) + 1;
+    for(int i = 2; i <= n;++i)
+    {
+        int fa = random(i - 1) + 1;
+        int val = random(inf) + 1;
+        cout << fa << " " << i << " " << val << endl;
+    }
+}
+
+inline tu()
+{
+    pair<int,int> e[1000005];
+    map<pair<int ,int > ,bool> h;
+    int n = random(10) + 1;
+    int m = random(100000) + 1;
+    for(int i = 1; i <= n;++i)
+    {
+        int fa = random(i - 1) + 1;
+        e[i] = make_pair(fa , i+1);
+        h[e[i]] = h[make_pair(i+1,fa)] = 1;
+    }
+    for(int i = n;i <= m;i++)
+    {
+        int x,y;
+        do{
+            x = random(n) + 1,y = random(n) + 1;
+        }while(x == y || h[make_pair(y,x)]);
+        random_shuffle(e+1,e+m+1);
+        for(int i = 1;i <= m;i++) 
+            cout << e[i].first << " " << e[i].second << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     srand((unsigned)time(0));
@@ -45,6 +79,10 @@ int main(int argc, char const *argv[])
     //listed();
     
     //interval();
+
+    //tree();
+
+    tu();
 
     return 0;
 }
