199dda648c4fb039b349cbb2f138998e2b68fe4d
Chicago <chicago01@qq.com>
2018-10-17 13:27:25 +0800

    卡特兰数

---
+++ 刷题/洛谷刷题/模拟/P1044栈.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+#define Are using namespace std;
+#define you int ans[19]={1,1, 2, 5, 14, 42, 132, 429, 1430, 4862, 16796, 58786, 208012, 742900, 2674440, 9694845, 35357670, 129644790, 477638700};int n;
+#define pig int main(void){cin>>n;cout<<ans[n];return 0;}
+
+Are you pig
