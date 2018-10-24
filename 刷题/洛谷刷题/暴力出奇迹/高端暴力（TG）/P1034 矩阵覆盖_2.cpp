08f86e40027bc5daa1a5d83071d8e32cbc1892f6
Chicago <chicago01@qq.com>
2018-10-24 19:03:31 +0800

    更新博客

--- 刷题/洛谷刷题/暴力出奇迹/高端暴力（TG）/P1034 矩阵覆盖_2.cpp
+++ 刷题/洛谷刷题/暴力出奇迹/高端暴力（TG）/P1034 矩阵覆盖_2.cpp
@@ -1,22 +1,26 @@
 #include<cstdio>
 #include<iostream>
 using namespace std;
-struct dot
+
+struct maps
 {
     int x,y;
-} a[51];
-struct mtrx
+} mapp[51];
+struct ss
 {
     int l,r,u,d;
     bool flag;
 } p[5];
+
 int n,m,ans=0x7f7f7f7f;
-bool in(mtrx a, int x, int y)
+
+bool in(ss a, int x, int y)
 {
     if (x>=a.l&&x<=a.r&&y>=a.d&&y<=a.u) return 1;
     return 0;
 }
-bool judge(mtrx a, mtrx b)
+
+bool judge(ss a, ss b)
 {
     if (in(a,b.l,b.u)) return 1;
     if (in(a,b.l,b.d)) return 1;
@@ -24,6 +28,7 @@ bool judge(mtrx a, mtrx b)
     if (in(a,b.r,b.d)) return 1;
     return 0;
 }
+
 void dfs(int num)
 {
     int value=0;
@@ -36,36 +41,42 @@ void dfs(int num)
       }
       value+=(p[i].r-p[i].l)*(p[i].u-p[i].d);
     }
+
     if (value>=ans) return;
+
     if (num>n){
         ans=value;
         return;
     }
+
     for (int i=1; i<=m; i++)
     {
-        mtrx tmp=p[i]; //tmp的初值 
+        ss tmp=p[i]; //tmp的初值 
         if (p[i].flag==0)
         {
             p[i].flag=1;
-            p[i].l=p[i].r=a[num].x;
-            p[i].u=p[i].d=a[num].y;
+            p[i].l=p[i].r=mapp[num].x;
+            p[i].u=p[i].d=mapp[num].y;
             dfs(num+1); p[i]=tmp;    //回溯 （一定要回溯彻底，用tmp变量） 
             break;
         }
         else 
         {
-            p[i].r=max(p[i].r,a[num].x); p[i].l=min(p[i].l,a[num].x);
-            p[i].u=max(p[i].u,a[num].y); p[i].d=min(p[i].d,a[num].y);
+            p[i].r=max(p[i].r,mapp[num].x); p[i].l=min(p[i].l,mapp[num].x);
+            p[i].u=max(p[i].u,mapp[num].y); p[i].d=min(p[i].d,mapp[num].y);
             dfs(num+1);
             p[i]=tmp;   //回溯 （一定要回溯彻底，用tmp变量） 
         }    
     }
 }
-int main()
+
+int main(void)
 {
     scanf("%d%d",&n,&m);
-    for (int i=1; i<=n; i++) scanf("%d%d",&a[i].x,&a[i].y);
+    for (int i=1; i<=n; i++) scanf("%d%d",&mapp[i].x,&mapp[i].y);
+
     dfs(1);
     printf("%d",ans);
+
     return 0;
 }
