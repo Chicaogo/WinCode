9797845ba75a1e76c3c7c540dfda603ac1297db8
Chicago <chicago01@qq.com>
2018-10-26 10:26:16 +0800

    文件夹整理

---
+++ 刷题/text.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    
+    cout<<"2333哈哈哈"<<endl;
+    
+    getchar();getchar();getchar();
+    return 0;
+}
+
+1. fa[100010] 父亲节点
+2. wa[100010] 敌人
+3. n,m n个罪犯 m对仇恨
+
+结构体 : x,y,w;
+重载小于号，从大到小排列;
+
+
+1. 查找函数
+2. 判断函数
+3. 合并函数
