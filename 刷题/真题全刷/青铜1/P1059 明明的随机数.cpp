ca1ddf21a4ddda00cbd7c75f7dc1413f5a85de46
Chicago <chicago01@qq.com>
2018-12-30 13:55:30 +0800

    老代码

---
+++ 刷题/真题全刷/青铜1/P1059 明明的随机数.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    set<int> ss;
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        cin>>num;
+        ss.insert(num);
+    }
+    cout<<ss.size()<<endl;
+    for(set<int>::iterator it=ss.begin();it != ss.end();it++){
+        cout<<*it<<" ";
+    }
+    return 0;
+}
