495988085faf9f609ee0a47881da98c501f40167
Chicago <chicago01@qq.com>
2019-01-29 02:03:01 +0800

    2:02

---
+++ 校内比赛/18-19年寒假新乡市友谊赛(普及-)(第二期)/T1 解二元一次方程组.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a1,a2,b1,b2,c1,c2;
+double x,y,cnt,cntx,cnty;
+
+void init()
+{
+    cin >> a1 >> b1 >> c1;
+    cin >> a2 >> b2 >> c2;
+    c1 = -c1;
+    c2 = -c2;
+}
+
+void find()
+{
+    //¿ËÀ³Ä·¹æÔò
+    cnt = a1*b2 - a2*b1;
+    cntx = c1*b2 - b1*c2;
+    cnty = a1*c2 - a2*c1;
+    if(cnt == 0) cout << "No answer£¡"
+    else printf("%.2lf %.2lf",cntx/cnt,cnty/cnt);
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+
+    init();
+
+    find();
+
+    getchar();getchar();getchar();
+    return 0;
+}
