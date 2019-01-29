40cbdc81b36a375247b88b085d86d2cfda3cca8f
Chicago <chicago01@qq.com>
2019-01-29 22:29:47 +0800

    数据修复

--- 校内比赛/18-19年寒假新乡市友谊赛(普及-)(第二期)/T1 解一元二次方程/T1 解二元一次方程组.cpp
+++ 校内比赛/18-19年寒假新乡市友谊赛(普及-)(第二期)/T1 解一元二次方程/T1 解二元一次方程组.cpp
@@ -18,20 +18,19 @@ void find()
     cnt = a1*b2 - a2*b1;
     cntx = c1*b2 - b1*c2;
     cnty = a1*c2 - a2*c1;
-    if(cnt == 0) cout << "No answer£¡";
+    if(cnt == 0) cout << "No answer";
     else printf("%.2lf %.2lf",cntx/cnt,cnty/cnt);
 }
 
 int main(int argc, char const *argv[])
 {
     ios::sync_with_stdio(false);
-    freopen("3.in","r",stdin);
-	freopen("3.out","w",stdout); 
+    freopen("2.in","r",stdin);
+	freopen("2.out","w",stdout); 
 
     init();
 
     find();
 
-    getchar();getchar();getchar();
     return 0;
 }
