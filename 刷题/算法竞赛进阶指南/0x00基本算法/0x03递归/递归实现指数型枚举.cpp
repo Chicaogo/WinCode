25f92df764dec32bb073c906dd05fda76a78e5c6
Chicago <chicago01@qq.com>
2019-01-31 09:43:27 +0800

    增加注释

--- 刷题/算法竞赛进阶指南/0x00基本算法/0x03递归/递归实现指数型枚举.cpp
+++ 刷题/算法竞赛进阶指南/0x00基本算法/0x03递归/递归实现指数型枚举.cpp
@@ -6,6 +6,7 @@ vector<int> num;
 
 void dfs(int k)
 {
+    //到达枚举边界，输出结果并结束
     if(k == n + 1)
     {
         for(int i = 0;i < num.size();++i)
@@ -15,10 +16,13 @@ void dfs(int k)
         return;
     }
 
+    //不选择这个数
     dfs(k+1);
 
+    //选择这个数
     num.push_back(k);
     dfs(k+1);
+    //回溯
     num.pop_back();
 }
 
