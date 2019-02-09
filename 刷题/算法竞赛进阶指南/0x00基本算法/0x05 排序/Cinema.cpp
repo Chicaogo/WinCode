4c6c9c036ffcf0450ecf83947fee21815bc833cc
Chicago <chicago01@qq.com>
2019-02-09 16:06:00 +0800

    AC

---
+++ 刷题/算法竞赛进阶指南/0x00基本算法/0x05 排序/Cinema.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 200010;
+int n,mm;
+unordered_map<int, int> m;
+
+struct node {
+    int pos,lan,sub;
+    bool operator <(const node &num) const
+    {
+        if(lan == num.lan) return sub > num.sub;
+        else return lan > num.lan;
+    }
+}a[MAX];
+
+inline void init()
+{
+    cin >> n;
+    for(int i = 1;i <= n;++i)
+    {
+        int x;
+        cin >> x;
+        m[x]++;
+    }
+
+    cin >> mm;
+    for(int i = 1;i <= mm;++i)
+    {
+        int x;
+        cin >> x;
+        a[i].pos = i;
+        a[i].lan = m[x];
+    }
+
+    for(int i = 1;i <= mm;++i)
+    {
+        int x;
+        cin >> x;
+        a[i].sub = m[x];
+    }
+}
+
+inline void find()
+{
+    sort(a+1,a+mm+1);
+    cout << a[1].pos;
+}
+
+int main(void)
+{
+    init();
+
+    find();
+
+    return 0;
+}
