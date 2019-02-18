5454c34143ea3cdf3639131f530d343bdb43263c
Chicago <chicago01@qq.com>
2019-02-18 23:34:08 +0800

    模板

---
+++ 比赛/CF1117/B.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void init()
+{
+
+}
+
+void find()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+
+    init();
+
+    find();
+
+    return 0;
+}
